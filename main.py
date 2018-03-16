bbe43133242f6e639f588fe142051833daef3528
Joseph Stanton <jsromejoe@gmail.com>
2018-03-15 20:44:08 -0400

    fairly minor tweaks and added bot auth

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import datetime
-from pytz import timezone
-import discord
-import asyncio
-from dateutil import tz
+import json
 import re
+
+import discord
+from dateutil import tz
+
+import gpxpy.gpx
+from shapely.geometry import Polygon, Point
+
+UticaExRaidEligible_channel_id = "423577070610808852"
 
 from_zone = tz.gettz('UTC')
 to_zone = tz.gettz('America/New_York')
 
-import gpxpy
-import gpxpy.gpx
-from shapely.geometry import Polygon, Point
+have_notified_set = []
+
+message_type_re = re.compile("")
+lat_re = re.compile('#(-?[0-9]+\.[0-9]+)')
+lon_re = re.compile(',(-?[0-9]+\.[0-9]+)')
+
+exraid_locations = []
+
 
 with open('Utica.gpx', 'r') as gpx_file:
     gpx = gpxpy.parse(gpx_file)
 
-exraid_locations = []
+
 for trk in gpx.tracks:
     seg = trk.segments[0]
     poly_points = []
@@ -25,13 +35,9 @@ for trk in gpx.tracks:
 
 client = discord.Client()
 
-message_type_re = re.compile("")
-lat_re = re.compile('#(-?[0-9]+\.[0-9]+)')
-lon_re = re.compile(',(-?[0-9]+\.[0-9]+)')
-
 
 def parse_message(msg):
-    #info = {'timestamp': timezone("US/Eastern").localize(msg.timestamp)}
+    # info = {'timestamp': timezone("US/Eastern").localize(msg.timestamp)}
     info = {'timestamp': msg.timestamp.replace(tzinfo=from_zone).astimezone(to_zone)}
 
     embeds = msg.embeds[0]
@@ -42,7 +48,7 @@ def parse_message(msg):
     desc_parts = embeds['description'].split('\n')
 
     info['Gym'] = desc_parts[0].replace("**", "")
-    #re.findall("'([a-zA-Z0-9 '.]+)'", desc_parts[0])[0]
+    # re.findall("'([a-zA-Z0-9 '.]+)'", desc_parts[0])[0]
     if 'is starting soon' in embeds['title']:
         info['type'] = 'egg'
         time_parts = list(map(lambda x: float(x), re.findall('([0-9]+)', desc_parts[1])))
@@ -61,22 +67,41 @@ def parse_message(msg):
     return info
 
 
+def make_string(info):
+    tmp = {}
+    for key, v in info.items():
+        tmp[key] = str(v)
+    return json.dumps(tmp)
+
+
+def have_notified(info):
+    s = make_string(info)
+    if s in have_notified_set:
+        return True
+    have_notified_set.append(s)
+    while len(have_notified_set) > 100:
+        have_notified_set.pop()
+
+    return False
+
+
 def isExRaidPossible(info):
     p = Point(info['latitude'], info['longitude'])
     return any(x.contains(p) for x in exraid_locations)
 
 
 def isStillRelevant(info):
-    if info['type'] == 'egg' and info['start time'] < datetime.datetime.now():
+    now = datetime.datetime.utcnow().replace(tzinfo=from_zone).astimezone(to_zone)
+    if info['type'] == 'egg' and info['start time'] > now:
         return True
-    elif info['type'] == 'raid' and info['end time'] > datetime.datetime.now():
+    elif info['type'] == 'raid' and info['end time'] > now:
         return True
     else:
         return False
 
 
 async def forward_info(info):
-    target_channel = client.get_channel("419537554191286294")
+    target_channel = client.get_channel(UticaExRaidEligible_channel_id)
     em = None
     if info['type'] == 'egg':
         lat = str(info["latitude"])
@@ -104,25 +129,25 @@ async def forward_info(info):
                         inline=True)
         embed.add_field(name="Gym", value=info['Gym'], inline=True)
         embed.add_field(name="CP", value=info['cp'], inline=True)
-        embed.add_field(name="Moves", value="Fast Move: " + info['fast move'] + "\nCharged Move: " + info['charge move'], inline=True)
+        embed.add_field(name="Moves",
+                        value="Fast Move: " + info['fast move'] + "\nCharged Move: " + info['charge move'], inline=True)
         embed.add_field(name="Map",
                         value="https://www.google.com/maps/@" + lat + "," + lon + ",16z",
                         inline=True)
         em = embed
-        #em = discord.Embed(title="Raid has started!", type='rich')
+        # em = discord.Embed(title="Raid has started!", type='rich')
     await client.send_message(target_channel, "", embed=em)
-
-
 
 
 async def handle_message(message):
     if message.author.name != 'GymHuntrBot' or len(message.embeds) == 0:
         return
     info = parse_message(message)
-    if isExRaidPossible(info) and isStillRelevant(info):
+    if isExRaidPossible(info) and isStillRelevant(info) and not have_notified(info):
         print("Exraid thingy")
         print(info)
         await forward_info(info)
+
 
 @client.event
 async def on_ready():
@@ -131,19 +156,17 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-    print("here")
-
-    target_channel = client.get_channel("419537554191286294")
-    client.send_message(target_channel, "Bot Activated")
+    target_channel = client.get_channel(UticaExRaidEligible_channel_id)
+    await client.send_message(target_channel, "Bot Activated")
 
     channel = client.get_channel("334867495846412288")
 
-    async for message in client.logs_from(channel, limit=10):
+    async for message in client.logs_from(channel, limit=100):
         await handle_message(message)
 
     channel2 = client.get_channel("339208908331548673")
 
-    async for message in client.logs_from(channel2, limit=10):
+    async for message in client.logs_from(channel2, limit=100):
         await handle_message(message)
 
 
@@ -152,25 +175,11 @@ channels_to_watch = ["utica-legendary-raids", "utica-raids"]
 
 @client.event
 async def on_message(message):
-    if (message.server.name != 'Pokemon Go Raids 315'):
+    if message.server.name != 'Pokemon Go Raids 315':
         return
-    if (message.channel.name not in channels_to_watch):
+    if message.channel.name not in channels_to_watch:
         return
 
-    print(message)
-    handle_message(message)
-    # if message.content.startswith('!test'):
-    #    counter = 0
-    #    tmp = await client.send_message(message.channel, 'Calculating messages...')
-    #    async for log in client.logs_from(message.channel, limit=100):
-    #        if log.author == message.author:
-    #            counter += 1
+    await handle_message(message)
 
-
-#
-#    await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-# elif message.content.startswith('!sleep'):
-#    await asyncio.sleep(5)
-#    await client.send_message(message.channel, 'Done sleeping')
-
-#client.run()
+client.run("NDIzMjY1NDAwOTg2NTMzODk4.DYsXdg.SmV3WWlrEJuUgb8fg6csREbGI0E")
